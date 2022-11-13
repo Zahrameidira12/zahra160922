73ef1756dd7516dea07dc7c603043b51498dbfbf
Zahra Meidira <zahrameidira2352003@gmail.com>
2022-11-13 12:08:14 +0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zahra160922;
+
+/**
+ *
+ * @author HP
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World "+"MI2C "+"Pemograman Berbasis Objek\n");
+        System.out.println("nama \t:Zahra Meidira \nnobp \t:2101092026 \nprodi \t:MI \njurusan :TI \nalamat  :Padang Pariaman \n)");
+    }
+    
+}
